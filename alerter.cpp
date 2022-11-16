4e5d77697941c0ff0a4da1b809bf618e55314390
Ricardo Guerra <ricardo_gd2@hotmail.com>
2022-11-15 20:42:22 -0600

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -5,7 +5,7 @@ int alertFailureCount = 0;
 
 int main() {
     testFailureCount(400.5, alertFailureCount);
-    testFailureCount(303.6, alertFailureCount);
+    testFailureCount(303.5, alertFailureCount);
     assert(alertFailureCount == 1);
 
     std::cout << alertFailureCount << " alerts failed.\n";
