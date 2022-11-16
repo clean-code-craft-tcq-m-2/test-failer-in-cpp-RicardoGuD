8e63968859dbefd74c37433be69513e03a2f5912
Ricardo Guerra <ricardo_gd2@hotmail.com>
2022-11-15 20:44:52 -0600

    Update alerter.cpp
    Code from headers files added in main file due to linking problems

--- alerter.cpp
+++ alerter.cpp
@@ -1,12 +1,46 @@
 #include <iostream>
-#include "test_alerter.h"
+#include <assert.h>
+#include <functional>
+//#include "alerterTest.h"
 
-int alertFailureCount = 0;
+/*Condition added to be able to control the type of error code by changing the temperature*/
+int networkAlertStub(float celcius) {
+    std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
+    int errorCode = 200;
+    if (celcius >= 200) {
+        errorCode = 500;
+    }
+    // Return 200 for ok
+    // Return 500 for not-ok
+    // stub always succeeds and returns 200
+    return errorCode;
+}
+
+/*networkAlertStub passed as parameter so we can change it for the real function without change the production code*/
+void alertInCelcius(float farenheit, int& failureCount, std::function<int(float)>& networkAlertFunc) {
+    float celcius = (farenheit - 32) * 5 / 9;
+    int returnCode = networkAlertFunc(celcius);
+    if (returnCode != 200) {
+        // non-ok response is not an error! Issues happen in life!
+        // let us keep a count of failures to report
+        // However, this code doesn't count failures!
+        // Add a test below to catch this bug. Alter the stub above, if needed.
+        failureCount += 0;
+    }
+}
+
+/*testing function added to test properly the production code*/
+void testFailureCount(float temperatureValueFarenheit, int& alertFailureCount) {
+    std::function<int(float tempValFarenheit)> lNetworkAlertStub = nullptr;
+    lNetworkAlertStub = std::bind(networkAlertStub, std::placeholders::_1);
+    alertInCelcius(temperatureValueFarenheit, alertFailureCount, lNetworkAlertStub);
+}
 
 int main() {
-    testFailureCount(400.5, alertFailureCount);
-    testFailureCount(303.5, alertFailureCount);
-    assert(alertFailureCount == 1);
+    int alertFailureCount = 0;
+    testFailureCount(400.5, alertFailureCount); //error code 500
+    testFailureCount(303.5, alertFailureCount); //error code 200
+    assert(alertFailureCount == 1); //one failure should be counted 
 
     std::cout << alertFailureCount << " alerts failed.\n";
     std::cout << "All is well (maybe!)\n";
