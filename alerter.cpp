3ffeafd03d8ca502f5774254723bf7cb0596685e
Ricardo Guerra <ricardo_gd2@hotmail.com>
2022-11-15 20:07:05 -0600

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -1,32 +1,15 @@
 #include <iostream>
-#include <assert.h>
+#include "alerterTest.h"
 
 int alertFailureCount = 0;
 
-int networkAlertStub(float celcius) {
-    std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return 200;
-}
-
-void alertInCelcius(float farenheit) {
-    float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlertStub(celcius);
-    if (returnCode != 200) {
-        // non-ok response is not an error! Issues happen in life!
-        // let us keep a count of failures to report
-        // However, this code doesn't count failures!
-        // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
-    }
-}
-
 int main() {
-    alertInCelcius(400.5);
-    alertInCelcius(303.6);
+    testFailureCount(400.5, alertFailureCount);
+    testFailureCount(303.6, alertFailureCount);
+    assert(alertFailureCount == 1);
+
     std::cout << alertFailureCount << " alerts failed.\n";
     std::cout << "All is well (maybe!)\n";
+
     return 0;
 }
